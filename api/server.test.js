e7cecb16e63838f71505f673039a894e185c8bfc
Calvin K <calvinknaub@gmail.com>
2024-01-15 12:47:26 -0800

    register test

--- api/server.test.js
+++ api/server.test.js
@@ -1,8 +1,7 @@
 const request = require("supertest");
 const db = require("../data/dbConfig");
 const server = require("./server");
-
-const Person = { username: "Captain Marvel", password: "foobar" };
+const bcrypt = require("bcryptjs");
 
 beforeAll(async () => {
   await db.migrate.rollback();
@@ -11,8 +10,12 @@ beforeAll(async () => {
 beforeEach(async () => {
   await db.seed.run();
 });
+afterAll(async () => {
+  await db.destroy();
+});
 
 describe("[POST] /login", () => {
+  const Person = { username: "Captain Marvel", password: "foobar" };
   test("successful login with 200 ok", async () => {
     const res = await request(server).post("/api/auth/login").send(Person);
     expect(res.status).toBe(200);
@@ -24,6 +27,34 @@ describe("[POST] /login", () => {
       "Welcome back Captain Marvel..."
     );
     expect(res.body).toHaveProperty("token");
+  });
+});
+
+describe("[POST] /register", () => {
+  const newUser = { username: "dougy", password: "testpassword" };
+
+  test("adds a user to the database", async () => {
+    await request(server).post("/api/auth/register").send(newUser);
+    const users = await db("users");
+    expect(users).toHaveLength(2);
+  });
+
+  test("responds with the new user and matches hashed password", async () => {
+    const { body } = await request(server)
+      .post("/api/auth/register")
+      .send({
+        ...newUser,
+        password: bcrypt.hashSync(newUser.password, 8),
+      });
+
+    const userInDatabase = await db("users")
+      .where({ username: newUser.username })
+      .first();
+
+    expect(body).toMatchObject({
+      username: newUser.username,
+      password: userInDatabase.password,
+    });
   });
 });
 
